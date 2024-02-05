220487a997134bba8e257f1ef55ce9a485b08e24
Andrey Ramírez <132618243+AndRM99@users.noreply.github.com>
2024-02-05 13:07:36 -0600

    Creación de ejemplo de Laravel Components
    Creación de ejemplo de Laravel Components
    - Creating components

---
+++ components/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //De esta mamera da error, ya que esto sugiere que Laravel no está reconociendo la función factory()
+        //factory(class:'App\Models\User', amount: 10)->create();
+
+        //forma correcta 
+        User::factory(10)->create();
+
+
+
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ components/resources/views/components/side-widget.blade.php
@@ -0,0 +1,6 @@
+<!-- Side Widget -->
+<div class="card my-4">
+    <h5 class="card-header">Side Widget</h5>
+    <div class="card-body">
+    new Bootstrap 4 card containers!
+    </div>
